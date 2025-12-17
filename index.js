40690408277c34654b87f5f126e291ca77d42d67
Антон <mikolenko.anton1@gmail.com>
2025-12-18 00:59:37 +0200

    index.js

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-// Токен беремо з env TG_TOKEN (Railway → Variables)
-const token = process.env.TG_TOKEN;
+// Тимчасово тримаємо токен прямо в коді
+const token = '8413003519:AAHLrlYJZPRFeSyslhQalYNS5Uz5qh8jZn8';
 const chatId = -1003348454247; // твій chat_id групи
-
-if (!token) {
-  console.error('TG_TOKEN is not set');
-  process.exit(1);
-}
 
 const bot = new TelegramBot(token, { polling: true });
 
 // Повідомлення при запуску
-bot.sendMessage(chatId, '⚡️ Світло Плаза Квартал: бот запущений (через TG_TOKEN)');
+bot.sendMessage(chatId, '⚡️ Світло Плаза Квартал: бот запущений');
 
 // Проста команда для перевірки
 bot.on('message', (msg) => {
